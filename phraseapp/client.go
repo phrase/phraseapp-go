3421e539cd530330358d8d941ab82c56a4e76fb9
fredv <f@dynport.de>
2015-08-15 18:17:57 +0200

    makes client embed http.Client

--- phraseapp/client.go
+++ phraseapp/client.go
@@ -2,26 +2,38 @@ package phraseapp
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 
 	"os"
 
 	"github.com/bgentry/speakeasy"
 )
 
+var Debug bool
+
+func EnableDebug() {
+	Debug = true
+}
+
 type DefaultParams Action
 type Action map[string]map[string]interface{}
 
 type Client struct {
+	http.Client
 	Credentials *Credentials
 }
 
 type Credentials struct {
-	Username string `cli:"opt --username desc='username used for authentication'"`
-	Token    string `cli:"opt --access-token desc='access token used for authentication'"`
-	TFA      bool   `cli:"opt --tfa desc='use Two-Factor Authentication'"`
-	Host     string `cli:"opt --host desc='Host to send Request to'"`
-	Debug    bool   `cli:"opt --verbose desc='Verbose output'"`
+	Username string
+	Token    string
+	TFA      bool
+	Host     string
+	Debug    bool
 }
 
 func NewClient(credentials Credentials, defaultCredentials *Credentials) (*Client, error) {
@@ -107,4 +119,84 @@ func (ah *Credentials) validate() error {
 	default:
 		return nil
 	}
+}
+
+func (client *Client) sendRequestPaginated(method, rawurl, ctype string, r io.Reader, status, page, perPage int) (io.ReadCloser, error) {
+	endpointUrl := client.Credentials.Host + rawurl
+	u, err := url.Parse(endpointUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	query := u.Query()
+	query.Add("page", strconv.Itoa(page))
+	query.Add("per_page", strconv.Itoa(perPage))
+
+	u.RawQuery = query.Encode()
+
+	if Debug {
+		fmt.Fprintln(os.Stderr, method, u.String())
+	}
+
+	req, err := http.NewRequest(method, u.String(), r)
+	if err != nil {
+		return nil, err
+	}
+
+	if ctype != "" {
+		req.Header.Add("Content-Type", ctype)
+	}
+
+	resp, err := client.send(req, status)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Body, nil
+}
+
+func (client *Client) sendRequest(method, url, ctype string, r io.Reader, status int) (io.ReadCloser, error) {
+	endpointUrl := client.Credentials.Host + url
+	if Debug {
+		fmt.Fprintln(os.Stderr, method, url)
+		bytes, err := ioutil.ReadAll(r)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s", err.Error())
+		}
+		str := string(bytes)
+		fmt.Fprintln(os.Stderr, str)
+		r = strings.NewReader(str)
+	}
+	req, err := http.NewRequest(method, endpointUrl, r)
+	if err != nil {
+		return nil, err
+	}
+
+	if ctype != "" {
+		req.Header.Add("Content-Type", ctype)
+	}
+
+	resp, err := client.send(req, status)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body, nil
+}
+
+func (client *Client) send(req *http.Request, status int) (*http.Response, error) {
+	err := client.authenticate(req)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	err = handleResponseStatus(resp, status)
+	if err != nil {
+		resp.Body.Close()
+	}
+	return resp, err
 }
