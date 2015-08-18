c1a50f1d5176065c2c7a1437c23bd00d0f386dc6
fredv <f@dynport.de>
2015-08-18 13:41:30 +0200

    1.0.0.rc16
    blacklisted_keys/list

--- phraseapp/lib.go
+++ phraseapp/lib.go
@@ -2991,5 +2991,5 @@ func (client *Client) VersionsList(project_id, translation_id string, page, perP
 }
 
 func GetUserAgent() string {
-	return "PhraseApp go (test)"
+	return "PhraseApp go (1.0.0.rc16)"
 }
