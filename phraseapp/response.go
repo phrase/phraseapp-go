fba50f5910bf43436d896e5d471a547583ab53d4
fredv <f@dynport.de>
2015-08-18 18:34:17 +0200

    adds nicer error messages

--- phraseapp/response.go
+++ phraseapp/response.go
@@ -6,6 +6,12 @@ import (
 	"net/http"
 )
 
+const docsURL = `http://docs.phraseapp.com/api/v2/`
+
+func further() string {
+	return fmt.Sprintf("\nFor further information see:\n%s", docsURL)
+}
+
 func handleResponseStatus(resp *http.Response, expectedStatus int) error {
 	switch resp.StatusCode {
 	case expectedStatus:
@@ -17,8 +23,12 @@ func handleResponseStatus(resp *http.Response, expectedStatus int) error {
 			return err
 		}
 		return e
+	case 401:
+		return fmt.Errorf("401 - %s\nThe credentials you provided are invalid.%s", http.StatusText(resp.StatusCode), further())
+	case 403:
+		return fmt.Errorf("403 - %s\nYou are not authorized to perform the requested action on the requested resource. Check if your provided access_token has the correct scope.%s", http.StatusText(resp.StatusCode), further())
 	case 404:
-		return fmt.Errorf("not found")
+		return fmt.Errorf("404 - Resource Not Found\nThe resource you requested or referenced resources you required do either not exist or you do not have the authorization to request this resource.")
 	case 422:
 		e := new(ValidationErrorResponse)
 		err := json.NewDecoder(resp.Body).Decode(&e)
@@ -33,6 +43,6 @@ func handleResponseStatus(resp *http.Response, expectedStatus int) error {
 		}
 		return e
 	default:
-		return fmt.Errorf("unexpected status code (%d) received; expected %d", resp.StatusCode, expectedStatus)
+		return fmt.Errorf("Unexpected HTTP Status Code (%d %s) received; expected %d %s.%s", resp.StatusCode, http.StatusText(resp.StatusCode), expectedStatus, http.StatusText(expectedStatus), further())
 	}
 }
