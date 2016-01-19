2c5b93832fdb28eeaa770358e3eac247b7e72572
Gereon Frey <gereon.frey@dynport.de>
2016-01-19 09:26:53 +0100

    config: add parsing and validation code
    This code was previously defined in the client code. Now it located here
    for better reuse.

--- phraseapp/config.go
+++ phraseapp/config.go
@@ -30,45 +30,44 @@ func (cfg *Config) UnmarshalYAML(unmarshal func(i interface{}) error) error {
 		return err
 	}
 
-	var ok bool
-	cfgErrStr := "configuration key %q has invalid value\nsee https://phraseapp.com/docs/developers/cli/configuration/"
+	var err error
 	for k, v := range t.Phraseapp {
 		switch k {
 		// phraseapp.Credentials parameters:
 		case "access_token":
-			if cfg.Credentials.Token, ok = v.(string); !ok {
-				return fmt.Errorf(cfgErrStr, k)
+			if cfg.Credentials.Token, err = ValidateIsString(k, v); err != nil {
+				return err
 			}
 		case "host":
-			if cfg.Credentials.Host, ok = v.(string); !ok {
-				return fmt.Errorf(cfgErrStr, k)
+			if cfg.Credentials.Host, err = ValidateIsString(k, v); err != nil {
+				return err
 			}
 		case "debug":
-			if cfg.Credentials.Debug, ok = v.(bool); !ok {
-				return fmt.Errorf(cfgErrStr, k)
+			if cfg.Credentials.Debug, err = ValidateIsBool(k, v); err != nil {
+				return err
 			}
 		case "username", "tfa":
 			return fmt.Errorf("username and tfa not supported in config")
 		// ProjectID used if required.
 		case "project_id":
-			if cfg.ProjectID, ok = v.(string); !ok {
-				return fmt.Errorf(cfgErrStr, k)
+			if cfg.ProjectID, err = ValidateIsString(k, v); err != nil {
+				return err
 			}
 		case "page":
-			page, ok := v.(int)
-			if !ok {
-				return fmt.Errorf(cfgErrStr, k)
+			page, err := ValidateIsInt(k, v)
+			if err != nil {
+				return err
 			}
 			cfg.Page = &page
 		case "perpage":
-			perpage, ok := v.(int)
-			if !ok {
-				return fmt.Errorf(cfgErrStr, k)
+			perpage, err := ValidateIsInt(k, v)
+			if err != nil {
+				return err
 			}
 			cfg.PerPage = &perpage
 		case "file_format":
-			if cfg.FileFormat, ok = v.(string); !ok {
-				return fmt.Errorf(cfgErrStr, k)
+			if cfg.FileFormat, err = ValidateIsString(k, v); err != nil {
+				return err
 			}
 		// Special pull and push action configuration.
 		case "push":
@@ -85,28 +84,98 @@ func (cfg *Config) UnmarshalYAML(unmarshal func(i interface{}) error) error {
 			}
 		// Arbitrary command defaults.
 		case "defaults":
-			val, ok := v.(map[interface{}]interface{})
-			if !ok { return fmt.Errorf(cfgErrStr, k) }
+			val, err := ValidateIsRawMap(k, v)
+			if err != nil {
+				return err
+			}
 
 			cfg.Defaults = map[string]map[string]interface{}{}
-			for rawPath, rawConfig := range val {
-				path, ok := rawPath.(string)
-				if !ok { return fmt.Errorf(cfgErrStr, rawPath) }
-
-				config, ok := rawConfig.(map[interface{}]interface{})
-				if !ok { return fmt.Errorf(cfgErrStr, rawPath)}
-
-				cfg.Defaults[path] = map[string]interface{}{}
-				for rawKey, value := range config {
-					key, ok := rawKey.(string)
-					if !ok { return fmt.Errorf(cfgErrStr, key) }
-
-					cfg.Defaults[path][key] = value
+			for path, rawConfig := range val {
+				cfg.Defaults[path], err = ValidateIsRawMap("defaults." + path, rawConfig)
+				if err != nil {
+					return err
 				}
 			}
-		// ignore
 		default:
 			return fmt.Errorf("configuration key %q invalid\nsee https://phraseapp.com/docs/developers/cli/configuration/", k)
+		}
+	}
+
+	return nil
+}
+
+const cfgValueErrStr = "configuration key %q has invalid value\nsee https://phraseapp.com/docs/developers/cli/configuration/"
+const cfgKeyErrStr = "configuration key %q has invalid type\nsee https://phraseapp.com/docs/developers/cli/configuration/"
+const cfgInvalidKeyErrStr = "configuration key %q unknown\nsee https://phraseapp.com/docs/developers/cli/configuration/"
+
+func ValidateIsString(k string, v interface{}) (string, error) {
+	s, ok := v.(string)
+	if !ok {
+		return "", fmt.Errorf(cfgValueErrStr, k)
+	}
+	return s, nil
+}
+
+func ValidateIsBool(k string, v interface{}) (bool, error) {
+	b, ok := v.(bool)
+	if !ok {
+		return false, fmt.Errorf(cfgValueErrStr, k)
+	}
+	return b, nil
+}
+
+func ValidateIsInt(k string, v interface{}) (int, error) {
+	i, ok := v.(int)
+	if !ok {
+		return 0, fmt.Errorf(cfgValueErrStr, k)
+	}
+	return i, nil
+}
+
+func ValidateIsRawMap(k string, v interface{}) (map[string]interface{}, error) {
+	raw, ok := v.(map[interface{}]interface{})
+	if !ok {
+		return nil, fmt.Errorf(cfgValueErrStr, k)
+	}
+
+	ps := map[string]interface{}{}
+	for mk, mv := range raw {
+		s, ok := mk.(string)
+		if !ok {
+			return nil, fmt.Errorf(cfgKeyErrStr, fmt.Sprintf("%s.%v", k, mk))
+		}
+		ps[s] = mv
+	}
+	return ps, nil
+}
+
+func ParseYAMLToMap(unmarshal func(interface{}) error, keysWithType map[string]interface{})  error {
+	m := map[string]interface{}{}
+	if err := unmarshal(m); err != nil {
+		return err
+	}
+
+	var err error
+	for k, v := range m {
+		value, found := keysWithType[k]
+		if !found {
+			return fmt.Errorf(cfgInvalidKeyErrStr, k)
+		}
+
+		switch val := value.(type) {
+		case *string:
+			*val, err = ValidateIsString(k, v)
+		case *int:
+			*val, err = ValidateIsInt(k, v)
+		case *bool:
+			*val, err = ValidateIsBool(k, v)
+		case map[string]interface{}:
+			val, err = ValidateIsRawMap(k, v)
+		default:
+			err = fmt.Errorf(cfgValueErrStr, k)
+		}
+		if err != nil {
+			return err
 		}
 	}
 
