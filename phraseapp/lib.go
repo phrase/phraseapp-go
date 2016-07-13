d548c6e537a8d997d3a8fbda7d200a32b52df402
Tobias Schwab <tobias.schwab@dynport.de>
2016-07-13 10:44:37 +0200

    update lib

--- phraseapp/lib.go
+++ phraseapp/lib.go
@@ -14,8 +14,8 @@ import (
 )
 
 const (
-	RevisionDocs      = "3d576454cb1d04bb0938d3545c7fd2747e05c654"
-	RevisionGenerator = "2ddb71c1e7d67168c843b85febc7a1529470833a"
+	RevisionDocs      = "421bf264a691775ed8d6ad4f60a57718a6a9bcc9"
+	RevisionGenerator = "483343224c2927019ca20b5a09f08c03320a9b42"
 )
 
 type Account struct {
@@ -73,14 +73,16 @@ type Comment struct {
 }
 
 type Format struct {
-	ApiName         string `json:"api_name"`
-	DefaultEncoding string `json:"default_encoding"`
-	DefaultFile     string `json:"default_file"`
-	Description     string `json:"description"`
-	Exportable      bool   `json:"exportable"`
-	Extension       string `json:"extension"`
-	Importable      bool   `json:"importable"`
-	Name            string `json:"name"`
+	ApiName                   string `json:"api_name"`
+	DefaultEncoding           string `json:"default_encoding"`
+	DefaultFile               string `json:"default_file"`
+	Description               string `json:"description"`
+	Exportable                bool   `json:"exportable"`
+	Extension                 string `json:"extension"`
+	Importable                bool   `json:"importable"`
+	IncludesLocaleInformation bool   `json:"includes_locale_information"`
+	Name                      string `json:"name"`
+	RendersDefaultLocale      bool   `json:"renders_default_locale"`
 }
 
 type KeyPreview struct {
@@ -528,12 +530,14 @@ func (params *TranslationKeyParams) ApplyValuesFromMap(defaults map[string]inter
 }
 
 type LocaleParams struct {
-	Code           *string `json:"code,omitempty"  cli:"opt --code"`
-	Default        *bool   `json:"default,omitempty"  cli:"opt --default"`
-	Main           *bool   `json:"main,omitempty"  cli:"opt --main"`
-	Name           *string `json:"name,omitempty"  cli:"opt --name"`
-	Rtl            *bool   `json:"rtl,omitempty"  cli:"opt --rtl"`
-	SourceLocaleID *string `json:"source_locale_id,omitempty"  cli:"opt --source-locale-id"`
+	Code                        *string `json:"code,omitempty"  cli:"opt --code"`
+	Default                     *bool   `json:"default,omitempty"  cli:"opt --default"`
+	Main                        *bool   `json:"main,omitempty"  cli:"opt --main"`
+	Name                        *string `json:"name,omitempty"  cli:"opt --name"`
+	Rtl                         *bool   `json:"rtl,omitempty"  cli:"opt --rtl"`
+	SourceLocaleID              *string `json:"source_locale_id,omitempty"  cli:"opt --source-locale-id"`
+	UnverifyNewTranslations     *bool   `json:"unverify_new_translations,omitempty"  cli:"opt --unverify-new-translations"`
+	UnverifyUpdatedTranslations *bool   `json:"unverify_updated_translations,omitempty"  cli:"opt --unverify-updated-translations"`
 }
 
 func (params *LocaleParams) ApplyValuesFromMap(defaults map[string]interface{}) error {
@@ -580,6 +584,20 @@ func (params *LocaleParams) ApplyValuesFromMap(defaults map[string]interface{}) 
 				return fmt.Errorf(cfgValueErrStr, k, v)
 			}
 			params.SourceLocaleID = &val
+
+		case "unverify_new_translations":
+			val, ok := v.(bool)
+			if !ok {
+				return fmt.Errorf(cfgValueErrStr, k, v)
+			}
+			params.UnverifyNewTranslations = &val
+
+		case "unverify_updated_translations":
+			val, ok := v.(bool)
+			if !ok {
+				return fmt.Errorf(cfgValueErrStr, k, v)
+			}
+			params.UnverifyUpdatedTranslations = &val
 
 		default:
 			return fmt.Errorf(cfgInvalidKeyErrStr, k)
