d0f4227ded1f8f62e7b1b54b15350018db6e28d5
theSoenke <soenke.behrendt@gmail.com>
2018-09-10 18:09:38 +0200

    Test http_cache

--- phraseapp/http_cache_test.go
+++ phraseapp/http_cache_test.go
@@ -7,15 +7,28 @@ import (
 	"testing"
 )
 
-func TestGetReposHandler(t *testing.T) {
-	s := httptest.NewServer(http.HandlerFunc(index))
-	defer s.Close()
+func TestLocaleDownloadCaching(t *testing.T) {
+	var cached = false
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		url := r.URL.String()
+		if url == "/v2/projects/1/locales/1/download" {
+			if cached {
+				etag := r.Header.Get("If-None-Match")
+				if etag != "123" {
+					t.Errorf("etag should be '123' but is: '%s'", etag)
+				}
+			}
+			cached = true
+		}
+		w.Header().Set("Etag", "123")
+		io.WriteString(w, "OK")
+		return
+	}))
+	defer server.Close()
 
-	s.Po
-	http.Get(url)
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "OK")
-	return
+	client, _ := NewClient(Credentials{Host: server.URL}, false)
+	// TODO don't use user cache dir for cache
+	client.EnableCaching()
+	client.LocaleDownload("1", "1", &LocaleDownloadParams{})
+	client.LocaleDownload("1", "1", &LocaleDownloadParams{})
 }
